d25afbfdb75d41c31074012620f64814a6e8bac3
Coral272 <coral272@naver.com>
2023-04-24 21:03:13 +0900

    start

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#fbc531",
+  itemColor: "rgba(0, 0, 0, 0.5)",
+};
+
